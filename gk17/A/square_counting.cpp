f019b8eb648876ba8d7ee9a92d617cfaae6b5536
fadel <fadelsougou@gmail.com>
2021-07-08 19:04:46 +0200

    ...

--- gk17/A/square_counting.cpp
+++ gk17/A/square_counting.cpp
@@ -7,6 +7,7 @@ int count_squares(int R, int C){
     int ans = 0;
     for (int i=1; i < R; i++){
         for (int j=1; j < C; j++){
+            //ans += (j <= C-i-1)? j*(i+1-j) + j*(j+1)/2 : (j-C+i)*(C-j) + (C-i)*(i+1-j) + (C-i)*(C-i-1)/2;
             ans += min(i,j);
             for (int k=0; k<j; k++){
                 ans += (i+k-j >= C-j-1)? C-j-1 : max(i+k-j,0);
