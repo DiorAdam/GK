c66f008e715b48b7292819968052aae1f377a9c2
fadel <fadelsougou@gmail.com>
2021-07-11 11:22:48 +0200

    fix min_covering for parse sky

--- gk17/A/two_cubes.cpp
+++ gk17/A/two_cubes.cpp
@@ -6,25 +6,28 @@ using namespace std;
 
 
 int min_covering_interval(vector<int>& centers, vector<int> radii){
+    
     int m = 0, M=0;
     for (int i=1; i<centers.size(); i++){
-        if (centers[i] < centers[m]) m = i;
-        if (centers[i] > centers[M]) M = i;
+        if (centers[i] - radii[i] < centers[m] - radii[m]) m = i;
+        if (centers[i] + radii[M] > centers[M] + radii[M]) M = i;
     }
     int w = centers[M]-centers[m] + radii[M] + radii[m]; 
-    int ans = w/2, l = centers[m] + w/2, r = centers[M] - w/2;
+    int ans = w/2, l = centers[m]-radii[m] + w/2, r = centers[M]+radii[M] - w/2;
     if (w%2 == 1){
         l++; 
         r--;
         ans++;
     }
-    int surplus = 0;
+    int surplus = 0, ml=centers[m]-radii[m], mr=centers[M]+radii[M];
     for (int i=0; i<centers.size(); i++){
         if (centers[i] + radii[i] > l && centers[i] - radii[i] < r){
             surplus = max(surplus, min(centers[i]+radii[i]-l, r-centers[i]-radii[i]));
         }
+        if (centers[i] + radii[i] <= l && centers[i]+radii[i] > ml) ml = centers[i]+radii[i];
+        if (centers[i] - radii[i] >= r && centers[i]-radii[i] < mr) mr = centers[i]-radii[i];
     }
-    return ans + surplus;
+    return (surplus == 0)? max(ml-centers[m]+radii[m], centers[M]-mr+radii[M]) : ans + surplus;
 }
 
 
