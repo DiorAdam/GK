58115383f9d6428f8a533933b8b492dca3f74c38
fadel <fadelsougou@gmail.com>
2021-09-10 20:54:42 +0200

    feat high_buildings

---
+++ gk20/E/high_buildings.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+void skyline(int N, int A, int B, int C, vector<int>& v){
+    if (A-C+B > N || C > A || C > B) return;
+    if  (C == 0){
+        for (int i=0; i<A-1; i++){
+            v[i] = N-A+i+2;
+        }
+        for (int i=0; i<B-1; i++){
+            v[N-i-1] = N-B+i+2;
+        }
+        for (int i=A-1; i<N-B+1; i++){
+            v[i] = 1;
+        }
+    }
+    else if (C==1){
+        for (int i=0; i<A-1; i++){
+            v[i] = i+1;
+        }
+        if (B==1){
+            for (int i=A-1; i<N-1; i++){
+                v[i] = 1;
+            }
+            v[N-1] = N;
+        }
+        else{
+            v[A-1] = N;
+            for (int i=0; i<B-1; i++){
+                v[N-i-1] = i+2;
+            }
+            for (int i=A; i<N-B+1; i++){
+                v[i] = 1;
+            }
+        }
+    }
+    else {
+        for (int i=0; i<A-C; i++){
+            v[i] = i+1;
+        }
+        for (int i=0; i<C-1; i++){
+            v[A-C+i] = N;
+        }
+        for (int i=0; i<N-(A+B-C); i++){
+            v[A-1+i] = 1;
+        }
+        v[N-B+C-1] = N;
+        for (int i=0; i<B-C; i++){
+            v[N-1-i] = i+1;
+        }
+    }
+    return;
+}
+
+
+int main(){
+    string input;
+    cin >> input;
+    int T = stoi(input);
+    for (int t=1; t <= T; t++){
+        int N, A, B, C;
+        cin >> N;
+        cin >> A; 
+        cin >> B;
+        cin >> C;
+
+        vector<int> v(N, 0);
+        skyline(N, A, B, C, v);
+        if (v[0] == 0) 
+            cout << "case #" << t << ": " << "IMPOSSIBLE\n";
+        else{
+            cout << "case #" << t << ": ";
+            for (auto x : v){
+                cout << " " << x;
+            }
+            cout << endl;
+        }
+    }
+}
+
+
+
