d15cbdc4ea0c329b01ff68f481e2e203bc412575
fadel <fadelsougou@gmail.com>
2021-12-19 21:38:15 +0100

    using priority queues to sort the queries

--- gk20/H/boring_numbers.cpp
+++ gk20/H/boring_numbers.cpp
@@ -8,97 +8,6 @@ using namespace std;
 
 vector<int> digits_of_long(long n);
 int count_bn_size_1(int l, int r, bool even);
-long count_bn_same_size(const vector<int>& ld, const vector<int>& rd);
-long count_bn_of_sizes(int sz_l, int sz_r);
-long count_bn(long L, long R);
-
-vector<int> digits_of_long(long n){
-    vector<int> ans;
-    while(n>0){
-        ans.push_back((int) n%10);
-        n = n/10;
-    }
-    reverse(ans.begin(), ans.end());
-    return ans;
-}
-
-int count_bn_size_1(int l, int r, bool even){
-    int ans{0};
-    if (even){
-        for (int i=l; i<=r; i++)
-            if (i%2==0) ans++;
-    }
-    else{
-        for (int i=l; i<=r; i++)
-            if (i%2==1) ans++;
-    }
-    return ans;
-}
-
-long count_bn_same_size(const vector<int>& ld, const vector<int>& rd){
-    int n {(int) ld.size()}; 
-    int i{0}; 
-    while(i < n){
-        if (ld[i] < rd[i]){ 
-            int sz{n-i-1}; 
-            long ans{0}; 
-            ans += count_bn_size_1(ld[i]+1, rd[i]-1, (i+1)%2==0)*count_bn_of_sizes(sz, sz);
-            if (ld[i]%2 == (i+1)%2){ 
-                long ansl{1};
-                for (int j=i+1; j<n; j++) 
-                    ansl*=count_bn_size_1(ld[j], 9, (j+1)%2==0);
-                ans += ansl;
-            }
-            if (rd[i]%2 == (i+1)%2){
-                long ansr{1}; 
-                for (int j=i+1; j<n; j++) 
-                    ansr*=count_bn_size_1(0, rd[j], (j+1)%2==0); 
-                ans += ansr; 
-            }
-            return ans; 
-        }
-        else if(ld[i]%2 != (i+1)%2) 
-            return 0;
-        i++;
-    }
-    return 1; 
-}
-
-long count_bn_of_sizes(int sz_l, int sz_r){
-    long ans{0};
-    if (sz_r <=0) return 1;
-    for (int sz=sz_l; sz<= sz_r; sz++){
-        ans+= pow(5, sz);
-    }
-    return ans;
-}
-
-long count_bn(long L, long R){
-    vector<int> ld = digits_of_long(L);
-    vector<int> rd = digits_of_long(R);
-    if (ld.size() == rd.size()) 
-        return count_bn_same_size(ld, rd);
-    else{
-        int ls {(int) ld.size()}, rs {(int) rd.size()};
-        long ans{0};
-        vector<int> l9(ls, 9);
-        vector<int> r1(rs, 0); r1[0]=1;
-        return count_bn_same_size(ld, l9) + count_bn_of_sizes(ls+1, rs-1) + count_bn_same_size(r1, rd);
-    }
-}
-
-/*
-
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-
-using namespace std;
-
-
-vector<int> digits_of_long(long n);
-int count_bn_size_1(int l, int r, bool even);
 long count_bn_same_size(const vector<int>& ld, const vector<int>& rd, int s);
 long count_bn_of_sizes(int sz_l, int sz_r);
 long count_bn(long L, long R);
@@ -175,8 +84,6 @@ long count_bn(long L, long R){
     }
 }
 
-
-*/
 
 
 int main(){
--- gk20/H/friends_shortest_path.cpp
+++ gk20/H/friends_shortest_path.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
 
 using namespace std;
+
+
+bool friends(const string& a, const string& b){
+    for (int i=0; i<a.size(); i++){
+        for (int j=0; j<b.size(); j++){
+            if (a[i] == b[j]) return true;
+        }
+    }
+    return false;
+}
 
 
 
@@ -10,7 +23,36 @@ using namespace std;
 int main(){
     int T; cin >> T;
     for (int t=1; t <= T; t++){
-        
+        int N; cin >> N;
+        int Q; cin >> Q;
+        vector<string> names(N);
+        vector<vector<int>> graph;
+        unordered_map<int, int> graph;
+        for (int i=0; i<N; i++){
+            cin >> names[i];
+            vector<int> neighbs;
+            for (int j=0; j<i; j++){
+                if (friends(names[i], names[j])){
+                    neighbs.push_back(j);
+                    graph[j].push_back(i);
+                }
+            }
+            graph.push_back(neighbs);
+        }
+        vector<vector<int>> queries(Q, vector<int>(2,0));
+        unordered_map<int, int> occ;
+        for (int i=0; i<Q; i++){
+            cin >> queries[i][0];
+            cin >> queries[i][1];
+            occ[queries[i][0]]++;
+            occ[queries[i][1]]++;
+        }
+        auto comp = [](const pair<int, int>& a, const pair<int, int>& b) { 
+            return a.second < b.second; 
+        };
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> queue(occ.begin(), occ.end(), comp);
     }
 }
+
+
 
