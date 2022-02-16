416f5f5fef2dd66b5f956fee8bed225b5bd9b415
fadel <fadelsougou@gmail.com>
2022-02-16 15:42:25 +0100

    fixing off by one errors

--- gk21/B/longest_progression.cpp
+++ gk21/B/longest_progression.cpp
@@ -6,11 +6,10 @@ using namespace std;
 
 void inline longest_ending(int N, int* arr, int* mem, vector<int>& ends){
     for (int i=2; i<N; i++){
-        if (mem[i-1] == 1) mem[i] = 2;
-        else if (arr[i] - arr[i-1] == arr[i-1] - arr[i-2]) mem[i] = mem[i-1]+1;
+        if (arr[i] - arr[i-1] == arr[i-1] - arr[i-2]) mem[i] = mem[i-1]+1;
         else{
             ends.push_back(i-1);
-            mem[i] = 1;
+            mem[i] = 2;
         } 
     } 
     ends.push_back(N-1);
@@ -18,9 +17,8 @@ void inline longest_ending(int N, int* arr, int* mem, vector<int>& ends){
 
 void inline longest_starting(int N, int* arr, int* mem){
     for (int i=N-3; i>=0; i--){
-        if (mem[i+1] == 1) mem[i]=2;
-        else if (arr[i]-arr[i+1] == arr[i+1] - arr[i+2]) mem[i] = mem[i+1]+1;
-        else mem[i] = 1;
+        if (arr[i]-arr[i+1] == arr[i+1] - arr[i+2]) mem[i] = mem[i+1]+1;
+        else mem[i] = 2;
     }
 }
 
@@ -48,8 +46,8 @@ int main(){
             int new_sub_length = ll[j] +1;
             if (j<N-2 && arr[j+2] == arr[j]+2*period){
                 new_sub_length++;
+                if (j < N-3 && period == arr[j+3] - arr[j+2]) new_sub_length += lr[j+2]-1;
             }
-            if (j < N-2 && period == arr[j+3] - arr[j+2]) new_sub_length += lr[j+2]-1;
             ans = max(ans, new_sub_length);
         }
         cout << "Case #" << t << ": " << ans << '\n';
