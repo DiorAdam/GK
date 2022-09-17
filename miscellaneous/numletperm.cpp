69333d248a69f54243df4de0a32fee977d11e0c9
fadel <fadelsougou@gmail.com>
2022-09-17 11:58:31 +0200

    feat numlet permutations

---
+++ miscellaneous/numletperm.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+
+
+vector<int> numletperm(const string& s){
+    vector<int> ans(10, 0);
+    vector<string> numlet = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    vector<char> key = {'z', 'o', 'w', 'h', 'u', 'f', 'x', 'v', 'g', 'i'};
+    unordered_map<char, int> ss;
+    for (char c : s){
+        ss[c]++;
+    }
+    for (int i=0; i<=8; i+=2){
+        if (ss[key[i]] > 0){
+            ans[i] = ss[key[i]];
+            for (char c : numlet[i]){
+                if (ss[c] < ans[i]){
+                    ans[0] = -1;
+                    return ans;
+                }
+                ss[c] -= ans[i];
+            }
+        }
+    }
+    for (int i=1; i<=9; i+=2){
+        if (ss[key[i]] > 0){
+            ans[i] = ss[key[i]];
+            for (char c : numlet[i]){
+                if (ss[c] < ans[i]){
+                    ans[0] = -1;
+                    return ans;
+                }
+                ss[c] -= ans[i];
+            }
+        }
+    }
+    for (char c = 'a'; c<='z'; c++){
+        if (ss[c] != 0){
+            ans[0] = -1;
+            return ans;
+        }
+    }
+    return ans;
+}
+
+
+
+int main(){
+    string s = "fnienironxzeessvenenineoour";
+    vector<int> ans = numletperm(s);
+    if (ans[0] == -1)
+        cout << "not a permutation\n";
+    else{
+        for (int i=0; i<10; i++){
+            cout << ans[i] << "  ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+
